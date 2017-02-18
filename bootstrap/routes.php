0e75d0c17d8a8ccf01b3030ece56cddc42514fcc
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-18 15:39:12 +0100

    Added example routes

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -2,20 +2,18 @@
 
 use App\Service\RestRouter;
 
-$dir = __DIR__ . '/../src/App/Resources/routes/';
-
-$router = new RestRouter($container['router'], $settings['settings']['rest']);
+$router = new RestRouter($container['router'], $config['settings']['rest']);
 
 /**
- *        URL          |           CONTROLLER            |     ROUTE
- * --------------------|---------------------------------|----------------
- * GET /articles       | ArticleController:getArticle    | get_articles
- * GET /articles/:id   | ArticleController:getArticles   | get_article
- * POST /articles      | ArticleController:postArticle   | post_article
- * PUT /articles/:id   | ArticleController:putArticle    | put_article
- * DELETE /article/:id | ArticleController:deleteArticle | delete_article
+ *         URL          |           CONTROLLER            |     ROUTE
+ * ---------------------|---------------------------------|----------------
+ * GET /articles        | ArticleController:getArticle    | get_articles
+ * GET /articles/{id}   | ArticleController:getArticles   | get_article
+ * POST /articles       | ArticleController:postArticle   | post_article
+ * PUT /articles/{id}   | ArticleController:putArticle    | put_article
+ * DELETE /article/{id} | ArticleController:deleteArticle | delete_article
  */
-// $router->crud('articles', 'ArticleController');
+$router->CRUD('articles', 'ArticleController');
 
 // OR
 
@@ -24,3 +22,26 @@ $router = new RestRouter($container['router'], $settings['settings']['rest']);
 // $router->post('articles', 'ArticleController');
 // $router->put('articles', 'ArticleController');
 // $router->delete('articles', 'ArticleController');
+
+/***********************************************************/
+/* -------------------- SUB RESOURCES -------------------- */
+/***********************************************************/
+
+/**
+ *                URL                 |                   CONTROLLER                  |        ROUTE
+ * -----------------------------------|-----------------------------------------------|------------------------
+ * GET /articles/{id}/comments        | ArticleCommentController:getArticleComments   | get_article_comments
+ * GET /articles/{id}/comments/{id}   | ArticleCommentController:getArticleComment    | get_article_comment
+ * POST /articles/{id}/comments       | ArticleCommentController:postArticleComment   | post_article_comment
+ * PUT /articles/{id}/comments/{id}   | ArticleCommentController:putArticleComment    | put_article_comment
+ * DELETE /article/{id}/comments/{id} | ArticleCommentController:deleteArticleComment | delete_article_comment
+ */
+$router->subCRUD('articles', 'comments', 'ArticleCommentController');
+
+// OR
+
+// $router->cgetSub('articles', 'comments', 'ArticleController');
+// $router->getSub('articles', 'comments', 'ArticleController');
+// $router->postSub('articles', 'comments', 'ArticleController');
+// $router->putSub('articles', 'comments', 'ArticleController');
+// $router->deleteSub('articles', 'comments', 'ArticleController');
