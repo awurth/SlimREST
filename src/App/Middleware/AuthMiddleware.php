061bdec99013b285e768971a4a9975d6ed0a526c
Alexis Wurth <alexis.wurth57@gmail.com>
2017-02-24 23:11:32 +0100

    Check user role

--- src/App/Middleware/AuthMiddleware.php
+++ src/App/Middleware/AuthMiddleware.php
@@ -2,11 +2,25 @@
 
 namespace App\Middleware;
 
+use App\Exception\AccessDeniedException;
+use Interop\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class AuthMiddleware extends Middleware
 {
+    /**
+     * @var string
+     */
+    private $role;
+
+    public function __construct(ContainerInterface $container, $role = '')
+    {
+        parent::__construct($container);
+
+        $this->role = $role;
+    }
+
     public function __invoke(Request $request, Response $response, callable $next)
     {
         if (!$this->jwt->getAccessToken()) {
@@ -14,8 +28,14 @@ class AuthMiddleware extends Middleware
                 ->withStatus(401)
                 ->withJson([
                     'status' => 401,
-                    'message' => 'Access Denied'
+                    'message' => 'Unauthorized'
                 ]);
+        }
+
+        if ($this->role) {
+            if (!$this->jwt->getAccessToken()->user->inRole($this->role)) {
+                throw new AccessDeniedException('Access denied: User must have role ' . $this->role);
+            }
         }
 
         return $next($request, $response);
