948f9c8767ad6fc47842438ee9fa902335c9c941
Alexis Wurth <alexis.wurth57@gmail.com>
2017-04-03 10:46:42 +0200

    Use single quotes

--- src/App/Exception/AccessDeniedException.php
+++ src/App/Exception/AccessDeniedException.php
@@ -6,7 +6,7 @@ use Exception;
 
 class AccessDeniedException extends Exception
 {
-    public function __construct($message = "Access denied", $code = 0, Exception $previous = null)
+    public function __construct($message = 'Access denied', $code = 0, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
