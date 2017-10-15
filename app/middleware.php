be9515a406df6c7226ef18b2b959d4ec3e800109
Alexis Wurth <alexis.wurth57@gmail.com>
2017-10-15 14:42:03 +0200

    Registered AuthMiddleware in the container

--- app/middleware.php
+++ app/middleware.php
@@ -1,4 +1,14 @@
 <?php
 
-$app->add(new App\Middleware\JWTMiddleware($container['jwt']));
-$app->add(new App\Middleware\CORSMiddleware($container['cors']));
+use App\Middleware\AuthMiddleware;
+use App\Middleware\CORSMiddleware;
+use App\Middleware\JWTMiddleware;
+
+$container['auth.middleware'] = function ($container) {
+    return function ($role = null) use ($container) {
+        return new AuthMiddleware($container['auth'], $role);
+    };
+};
+
+$app->add(new JWTMiddleware($container['jwt']));
+$app->add(new CORSMiddleware($container['cors']));
--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,6 @@
 
 use App\Service\RestRouter;
 
-use App\Middleware\AuthMiddleware;
-
 $router = new RestRouter($container['router'], $config['rest']);
 
 /**
@@ -21,7 +19,7 @@ $app->group('/api', function () use ($container) {
     $this->post('/login', 'AuthController:login')->setName('login');
     $this->post('/auth/refresh', 'AuthController:refresh')->setName('jwt.refresh');
     $this->get('/users/me', 'AuthController:me')
-        ->add(new AuthMiddleware($container['sentinel']))
+        ->add($container['auth.middleware']())
         ->setName('users.me');
 });
 
