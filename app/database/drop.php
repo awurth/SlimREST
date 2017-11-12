5d6554da99fdba31cb9ab27c824ee58cf8363c43
Alexis Wurth <awurth.dev@gmail.com>
2017-11-12 20:34:26 +0100

    Added OAuth tables to drop list

--- app/database/drop.php
+++ app/database/drop.php
@@ -3,18 +3,31 @@
 use Illuminate\Database\Capsule\Manager;
 
 $tables = [
-    'activations',
-    'persistences',
-    'reminders',
-    'role_users',
-    'throttle',
-    'roles',
-    'access_token',
-    'refresh_token',
-    'user',
+    'sentinel' => [
+        'activations',
+        'persistences',
+        'reminders',
+        'role_users',
+        'throttle',
+        'roles',
+        'access_token',
+        'refresh_token',
+        'user',
+    ],
+    'oauth' => [
+        'oauth_clients',
+        'oauth_access_tokens',
+        'oauth_authorization_codes',
+        'oauth_refresh_tokens',
+        'oauth_scopes',
+        'oauth_jwt'
+    ],
+    'app' => []
 ];
 
 Manager::schema()->disableForeignKeyConstraints();
-foreach ($tables as $table) {
-    Manager::schema()->dropIfExists($table);
+foreach ($tables as $section => $sectionTables) {
+    foreach ($sectionTables as $table) {
+        Manager::schema()->dropIfExists($table);
+    }
 }
