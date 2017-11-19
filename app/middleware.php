23242ba00975a1640a15e78d89d2d172b8a8a9c5
Alexis Wurth <awurth.dev@gmail.com>
2017-11-19 15:33:29 +0100

    Registered the Authorization Middleware in the container

--- app/middleware.php
+++ app/middleware.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Security\Middleware\Authorization;
 use App\Security\Middleware\CorsMiddleware;
+
+$container['auth.middleware'] = function ($container) {
+    return new Authorization($container['oauth'], $container['sentinel']);
+};
 
 $app->add(new CorsMiddleware($container['config']['cors']));
