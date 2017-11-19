4fd3f0d829129933afc9539068576e316a93ff38
Alexis Wurth <awurth.dev@gmail.com>
2017-11-19 15:52:35 +0100

    Added app/bootstrap.php

--- public/index.php
+++ public/index.php
@@ -12,14 +12,6 @@ $container = $app->getContainer();
 
 $container['config'] = require __DIR__ . '/../app/config/config.php';
 
-require __DIR__ . '/../app/container.php';
-
-require __DIR__ . '/../app/handlers.php';
-
-require __DIR__ . '/../app/middleware.php';
-
-require __DIR__ . '/../app/controllers.php';
-
-require __DIR__ . '/../app/routes.php';
+require __DIR__ . '/../app/bootstrap.php';
 
 $app->run();
