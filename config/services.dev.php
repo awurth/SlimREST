a18fb0c7ec39dfc3291a0dda921c09451b677ba9
Alexis Wurth <awurth.dev@gmail.com>
2017-12-08 22:50:13 +0100

    Changed config dirs/files names

--- config/services.dev.php
+++ config/services.dev.php
@@ -2,7 +2,7 @@
 
 use Monolog\Logger;
 
-$config = require __DIR__ . '/config.php';
+$config = require __DIR__ . '/services.php';
 
 // Used for generating links in API routes markdown.
 $config['rest']['url'] = 'http://localhost/slim-rest-base';
